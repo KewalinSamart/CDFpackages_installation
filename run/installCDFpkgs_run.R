154b97933f4ab03241642bfa09054b6fdbe1fd91
Kewalin Samart <samartke@msu.edu>
2021-06-14 18:53:13 -0400

    Update installCDFpkgs_run.R

--- run/installCDFpkgs_run.R
+++ run/installCDFpkgs_run.R
@@ -2,12 +2,14 @@
 # created date: 06/14/21
 # Kewalin Samart
 
+library(stringr)
+
 source("R/installCDFpkgs_func.R")
 
 args <- commandArgs(TRUE)
 
 path_to_BAMfolder <- args[1]
 version <- args[2]
-organisms <- args[3]
+organisms <- str_split(args[3],",")
 
 download_entrezg(path_to_BAMfolder, version, organisms)
